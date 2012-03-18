b5449a71c5ea7032bff19aef22ecdcb962a84bcf
Lucas <lucas@War-Machine.local>
2012-03-18 12:46:18 -0300

    Initial Commit

---
+++ Proxie/Proxie.h
@@ -0,0 +1,15 @@
+//
+//  Proxie.h
+//  Proxie
+//
+//  Created by Lucas casteletti on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Proxie : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Proxie/Proxie.m
@@ -0,0 +1,17 @@
+//
+//  Proxie.m
+//  Proxie
+//
+//  Created by Lucas casteletti on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Proxie.h"
+
+@implementation Proxie
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
